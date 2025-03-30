bfee8a3eed66bc7723fc187a4b248d58f57cd6b5
Alejandro Contreras <alejandroalicontrerasdiaz@gmail.com>
2025-03-30 01:18:06 -0300

    fix typos

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,24 +1,11 @@
-// pages/auth/signin.tsx
-import { getProviders, signIn } from 'next-auth/react';
-import { Button } from '@/components/ui/button';
-
-export default function SignIn({ providers }: { providers: any }) {
+export  function SignIn() {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-900">
             <div className="bg-gray-800 p-8 rounded-lg shadow-lg max-w-md w-full">
                 <h1 className="text-2xl font-bold text-white mb-6 text-center">
                     Login to Spotify
                 </h1>
-                {Object.values(providers).map((provider: any) => (
-                    <div key={provider.name} className="mt-4">
-                        <Button
-                            onClick={() => signIn(provider.id, { callbackUrl: '/' })}
-                            className="w-full bg-green-500 hover:bg-green-600 text-white"
-                        >
-                            Continue with {provider.name}
-                        </Button>
-                    </div>
-                ))}
+            
             </div>
         </div>
     );
